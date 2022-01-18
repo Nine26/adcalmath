4e427cc038597da745925b88c0cd1947eb8cb5b5
Nine26 <stufffy88@gmail.com>
2022-01-18 13:39:25 +0400

    Update admathcal.go

--- admathcal.go
+++ admathcal.go
@@ -12,6 +12,7 @@ func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 
+	fmt.Printf("visit: https://github.com/Nine26/adcalmath for more updates")
 	fmt.Printf("\n1)volume of prism\n\n2)circles\n\n3)solving rational numbers with coefficients\n\n4)perimeter of various shapes\n\n5)area of various shapes\n\n00)exit\n")
 	fmt.Print("\npick a number: ")
 
@@ -334,7 +335,7 @@ func main() {
 
 	if input00 == 2 {
 
-		fmt.Printf("\n1)work out the circumfrence w radius\n\n2)work out the circumference w diameter\n\n3)work out radius with the circumference\n\n4)work out perimeter for different circles sizes\n\n5)work out perimeter for semicircle\n\n6)work out area of circle w radius\n\n7)work out area of circle w diameter\n\n8)work out radius of circle with area")
+		fmt.Printf("\n1)work out the circumfrence w radius\n\n2)work out the circumference w diameter\n\n3)work out radius with the circumference\n\n4)work out perimeter for different circles sizes\n\n5)work out perimeter for semicircle\n\n6)work out area of circle w radius\n\n7)work out area of circle w diameter\n\n8)work out radius of circle with area\n\n9)area of cylinder with radius\n\n10)area of cylinder with radius")
 		fmt.Print("\n\nplease pick a number: ")
 
 		scanner.Scan()
@@ -642,6 +643,88 @@ func main() {
 
 		}
 
-	}
+		if input0 == 9 {
 
+			fmt.Print("radius?: ")
+
+			scanner.Scan()
+
+			input3, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+			fmt.Print("height?: ")
+
+			scanner.Scan()
+
+			input4, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+			var answer_notrounded = math.Pi * float64(input3) * float64(input3) * float64(input4)
+
+			fmt.Print("round answer to 1dp?\n1 = yes\n2 = no ")
+			fmt.Print("\npick a number: ")
+
+			scanner.Scan()
+
+			input5, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+			if input5 == 1 {
+
+				fmt.Println("\nanswer: ", math.Round(answer_notrounded*10)/10)
+				fmt.Println("\npress any key to close")
+				scanner.Scan()
+
+			}
+
+			if input5 == 2 {
+
+				fmt.Println("\nanswer: ", answer_notrounded)
+
+				fmt.Println("\npress any key to close")
+				scanner.Scan()
+
+			}
+
+		}
+		if input0 == 10 {
+
+			fmt.Print("diameter?: ")
+
+			scanner.Scan()
+
+			input3, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+			fmt.Print("height?: ")
+
+			scanner.Scan()
+
+			input4, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+			var answer = float64(input3) / 2
+			var answer_notrounded = math.Pi * float64(answer) * float64(answer) * float64(input4)
+
+			fmt.Print("round answer to 1dp?\n1 = yes\n2 = no ")
+			fmt.Print("\npick a number: ")
+
+			scanner.Scan()
+
+			input5, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+			if input5 == 1 {
+
+				fmt.Println("\nanswer: ", math.Round(answer_notrounded*10)/10)
+				fmt.Println("\npress any key to close")
+				scanner.Scan()
+
+			}
+
+			if input5 == 2 {
+
+				fmt.Println("\nanswer: ", answer_notrounded)
+
+				fmt.Println("\npress any key to close")
+				scanner.Scan()
+
+			}
+
+		}
+	}
 }
